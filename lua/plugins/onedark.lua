7180d0e390c3635a3bf6f66bd6db7b318790b45d
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2024-11-05 16:19:51 +0200

    Switch to onedark color scheme

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons"
+  },
+  { 
+    "navarasu/onedark.nvim", 
+    lazy = false,
+    name = "onedark",
+    priority = 1000,
+    config = function()
+      require('onedark').setup { 
+        style = 'darker'
+      }
+
+      vim.cmd.colorscheme("onedark")
+    end
+  }
+}
