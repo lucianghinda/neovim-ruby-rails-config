dbcd0e8ba303ed1d4da4b733678330d015bcd9ae
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2025-10-27 21:24:31 +0200

    Changed to new config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,31 +19,42 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.ruby_lsp.setup({
-        capabilities = capabilities
-      })
+      -- Configure ruby_lsp using the new vim.lsp.config API
+      vim.lsp.config.ruby_lsp = {
+        cmd = { 'ruby-lsp' },
+        filetypes = { 'ruby' },
+        root_markers = { 'Gemfile', '.git' },
+        capabilities = capabilities,
+      }
 
-         lspconfig.yamlls.setup({
-      capabilities = capabilities,
-      settings = {
-        yaml = {
-          schemas = {
-            ["https://www.rubyschema.org/i18n-tasks.json"] = "i18n-tasks.yml",
-            ["https://www.rubyschema.org/lefthook.json"] = "lefthook.yml",
-            ["https://www.rubyschema.org/rubocop.json"] = ".rubocop.yml",
-            ["https://www.rubyschema.org/i18n/locale.json"] = "locale/*.yml",
-            ["https://www.rubyschema.org/kamal/deploy.json"] = "deploy.yml",
-            ["https://www.rubyschema.org/packwerk/package.json"] = "package.yml",
-            ["https://www.rubyschema.org/rails/cache.json"] = "cache.yml",
-            ["https://www.rubyschema.org/rails/database.json"] = "database.yml",
-            ["https://www.rubyschema.org/rails/queue.json"] = "queue.yml",
-            ["https://www.rubyschema.org/rails/recurring.json"] = "recurring.yml",
-            ["https://www.rubyschema.org/rails/storage.json"] = "storage.yml",
+      -- Configure yamlls using the new vim.lsp.config API
+      vim.lsp.config.yamlls = {
+        cmd = { 'yaml-language-server', '--stdio' },
+        filetypes = { 'yaml', 'yaml.docker-compose' },
+        root_markers = { '.git' },
+        capabilities = capabilities,
+        settings = {
+          yaml = {
+            schemas = {
+              ["https://www.rubyschema.org/i18n-tasks.json"] = "i18n-tasks.yml",
+              ["https://www.rubyschema.org/lefthook.json"] = "lefthook.yml",
+              ["https://www.rubyschema.org/rubocop.json"] = ".rubocop.yml",
+              ["https://www.rubyschema.org/i18n/locale.json"] = "locale/*.yml",
+              ["https://www.rubyschema.org/kamal/deploy.json"] = "deploy.yml",
+              ["https://www.rubyschema.org/packwerk/package.json"] = "package.yml",
+              ["https://www.rubyschema.org/rails/cache.json"] = "cache.yml",
+              ["https://www.rubyschema.org/rails/database.json"] = "database.yml",
+              ["https://www.rubyschema.org/rails/queue.json"] = "queue.yml",
+              ["https://www.rubyschema.org/rails/recurring.json"] = "recurring.yml",
+              ["https://www.rubyschema.org/rails/storage.json"] = "storage.yml",
+            },
           },
         },
-      },
-    })
+      }
+
+      -- Enable the LSP servers
+      vim.lsp.enable('ruby_lsp')
+      vim.lsp.enable('yamlls')
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
